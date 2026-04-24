41760ebf5fbacb6e42f7e95abd008e586a61221c
disougie <adisougie@gmail.com>
2026-04-24 10:35:19 +0200

    v1.1

--- server/index.js
+++ server/index.js
@@ -20,6 +20,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/leaves', leaveRoutes);
 
+app.get("/keep-alive", (req, res) => {
+    res.json({ "message": "Server is alive" });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
