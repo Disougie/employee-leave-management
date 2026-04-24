6b945210b275824025b738c1307f9110f0e36342
disougie <adisougie@gmail.com>
2026-04-24 14:11:06 +0200

    v1.2

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const isProduction = import.meta.env.MODE === 'production';
+const productionURL = import.meta.env.VITE_API_BASE_URL ? `${import.meta.env.VITE_API_BASE_URL}/api` : '/api';
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: isProduction ? productionURL : 'http://localhost:5000/api',
 });
 
 // Add a request interceptor
